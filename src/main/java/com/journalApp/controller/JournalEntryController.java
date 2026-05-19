cc51f66a9ae578fa3080d9349a3c54830985892c
Karthik <karthiksaini7800@gmail.com>
2026-05-19 23:34:06 +0530

    Second commit

--- src/main/java/com/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/journalApp/controller/JournalEntryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class JournalEntryController {
 	
 	@GetMapping
 	public List<JournalEntry> getAll(){
+		return new ArrayList<>(journalEntry.values());
+	}
+	
+	@GetMapping("/getAll")
+	public List<JournalEntry> getAllEntries(){
 		return new ArrayList<>(journalEntry.values());
 	}
 	
@@ -40,5 +46,10 @@ public class JournalEntryController {
 	@DeleteMapping("/id/{myId}")
 	public JournalEntry deleteJournalEntryById(@PathVariable Long myId) {
 		return journalEntry.remove(myId);
+	}
+	
+	@PutMapping("/id/{myId}")
+	public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+		return journalEntry.put(myId,myEntry);
 	}
 }

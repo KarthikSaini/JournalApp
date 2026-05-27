04145489fd3256be4d190a74f74e194c3e05c0c0
Karthik <karthiksaini7800@gmail.com>
2026-05-28 02:22:33 +0530

    Transactional added in API

--- src/main/java/com/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/journalApp/controller/JournalEntryControllerV2.java
@@ -1,10 +1,7 @@
 package com.journalApp.controller;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import com.journalApp.entity.JournalEntry;

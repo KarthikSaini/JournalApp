02d09efa3721fb7d95ff20b2f2919fc124a8cad9
Karthik <karthiksaini7800@gmail.com>
2026-06-01 01:07:21 +0530

    Env variable has been setup

--- src/main/java/com/journalApp/config/MongoConfig.java
+++ src/main/java/com/journalApp/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package com.journalApp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,12 +10,18 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig {
+	
+	@Value("${spring.data.mongodb.userName}")
+	private String userName;
+	
+	@Value("${spring.data.mongodb.password}")
+	private String password;
 
     @Bean
     public MongoTemplate mongoTemplate() {
 
-//        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-    	MongoClient mongoClient = MongoClients.create("mongodb+srv://karthiksaini7800_db_user:ZhJTuEvZFrgAxBAr@cluster0.qxhux60.mongodb.net/?appName=Cluster0");
+    	String url = "mongodb+srv://"+userName+":"+password+"@cluster0.qxhux60.mongodb.net/?appName=Cluster0";
+    	MongoClient mongoClient = MongoClients.create(url);
 
         return new MongoTemplate(mongoClient, "journaldb");
     }

7a704d8f040365038d784ac055e7711cfbdaba17
Karthik <karthiksaini7800@gmail.com>
2026-06-15 23:54:05 +0530

    Working on UserDetail service

--- src/main/java/com/journalApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/journalApp/service/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 	
-//	Working on other project
+//	Working on other project Day 5
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
